e885c60de79bad1e17447c2de72d282e057e0255
Travis Montgomery <travisbm@gmail.com>
2015-09-23 14:54:31 -0500

    removed first and last name from user model. set up validation for both models. Last commit before migration.

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,3 +1,4 @@
 class Shout < ActiveRecord::Base
   belongs_to :user
+  validates :message, length: { in: 1..160 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-  has_many: shouts
+  has_many :shouts
+  validates :user_name, presence: true
 end
