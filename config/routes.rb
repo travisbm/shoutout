562f1d9088ebc1d95226e272f08d0f78b24defae
Travis Montgomery <travisbm@gmail.com>
2015-09-23 14:35:46 -0500

    generated controller for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/create'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/migrate/20150923192831_create_shouts.rb
+++ db/migrate/20150923192831_create_shouts.rb
@@ -1,7 +1,7 @@
 class CreateShouts < ActiveRecord::Migration
   def change
     create_table :shouts do |t|
-      t.string :message
+      t.string :message, limit: 160
       t.belongs_to :user, index: true, foreign_key: true
 
       t.timestamps null: false
